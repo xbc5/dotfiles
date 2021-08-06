94ce0316d423ff1b7e79865b6bbd70864b04e469
xbc5 <xbc5@/dev/null>
2021-08-06 03:01:00 +0100

    feat(nvim): add emmet-ls

--- nvim/.config/nvim/lua/config/nvim-lspinstall.lua
+++ nvim/.config/nvim/lua/config/nvim-lspinstall.lua
@@ -1,10 +1,13 @@
 return function()
+  local emmet = require("config.emmet-ls")
   local lspi = require('lspinstall')
 
+  emmet.create() -- create an installable emmet server, you must manually install it after
 
   local configs = {
     -- put any overrides you have in here, the key is the LSP 'server' name.
     lua = require("config.sumneko_lua"),
+    emmet = require("config.emmet-ls").config,
   }
   setmetatable(configs, { __index = function () return {} end }) -- empty table default
 
