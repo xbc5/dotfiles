b6b7359ab3c0c13bab78ef99452bede5476ae243
xbc5 <87829033+xbc5@users.noreply.github.com>
2021-11-08 11:55:19 +0000

    feat(nvim): add table merge helper

--- nvim/.config/nvim/lua/lib/table.lua
+++ nvim/.config/nvim/lua/lib/table.lua
@@ -12,5 +12,11 @@ function M.has(t)
   return function(value) return new[value] or false end
 end
 
+function M.merge(t1, t2)
+  for k,v in pairs(t2) do
+    t1[k] = v
+  end
+  return t1
+end
 
 return M
