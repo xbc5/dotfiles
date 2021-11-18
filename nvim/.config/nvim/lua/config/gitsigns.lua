417805fb4034fdf414218c887154f137d788b8e0
xbc5 <87829033+xbc5@users.noreply.github.com>
2021-11-18 17:18:55 +0000

    perf(nvim): disable gitsigns line blame (too heavy)

--- nvim/.config/nvim/lua/config/gitsigns.lua
+++ nvim/.config/nvim/lua/config/gitsigns.lua
@@ -1,5 +1,7 @@
 return function()
   require('gitsigns').setup {
-    current_line_blame = true,
+    -- generally speaking frequently polling the repo is a performance hog.
+    current_line_blame = false, -- HUGE performance hog, laggy input
+    update_debounce = 1000, -- does this really need to be 100ms (default)? use 1k instead
   }
 end
