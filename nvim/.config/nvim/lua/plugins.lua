68170b70fc1042b0bb8e2aad14301a77409b413c
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-01-03 00:17:11 +0000

    feat(nvim): add lorem ipsum plugin

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -42,6 +42,8 @@ return require('packer').startup({function(use)
 
     use 'wbthomason/packer.nvim' -- manage packer updates
 
+    use 'vim-scripts/loremipsum'
+
     use 'b3nj5m1n/kommentary'
     -- use 'takac/vim-hardtime'
 
